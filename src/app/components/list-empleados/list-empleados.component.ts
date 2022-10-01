d36ab24656d9c8473a3e10d29e3b460a9c4ef3ef
Sandra <dtena90@gmail.com>
2022-10-01 15:44:12 +0200

    Importar toolbar, input, icon, formFiled de Angular Material
    Importar toolbar, input, icon, formFiled de Angular Material + pintar el filtro y el nuevo menú en el componente list-empleados & en el componente de navbar.

--- src/app/components/list-empleados/list-empleados.component.ts
+++ src/app/components/list-empleados/list-empleados.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 
 export interface Empleados {
   position: number;
@@ -29,7 +30,7 @@ const ELEMENT_DATA: Empleados[] = [
 export class ListEmpleadosComponent implements OnInit {
 
   displayedColumns: string[] = ['position', 'name', 'age', 'dateIncorporation', 'generation'];
-  dataSource = ELEMENT_DATA;
+  dataSource = new MatTableDataSource(ELEMENT_DATA);
   clickedRows = new Set<Empleados>();
 
   constructor() { }
@@ -37,4 +38,8 @@ export class ListEmpleadosComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  applyFilter(event: Event) {
+    const filterValue = (event.target as HTMLInputElement).value;
+    this.dataSource.filter = filterValue.trim().toLowerCase();
+  }
 }
--- src/app/components/shared/angular-material/angular-material.module.ts
+++ src/app/components/shared/angular-material/angular-material.module.ts
@@ -10,6 +10,15 @@ import { MatToolbarModule } from '@angular/material/toolbar';
 //Importar Tabla de Material Angular
 import { MatTableModule } from '@angular/material/table';
 
+//Importal el form Field
+import { MatFormFieldModule } from '@angular/material/form-field';
+
+//Importar el input
+import { MatInputModule } from '@angular/material/input';
+
+//Importar los iconos
+import { MatIconModule } from '@angular/material/icon';
+
 
 
 @NgModule({
@@ -18,12 +27,18 @@ import { MatTableModule } from '@angular/material/table';
     CommonModule,
     MatSliderModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ],
   exports: [
     MatSliderModule,
     MatToolbarModule,
-    MatTableModule
+    MatTableModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatIconModule
   ]
 })
 export class AngularMaterialModule { }
