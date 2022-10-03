03fe3fe101479858c633c583a913b9633ba35c0e
Sandra <dtena90@gmail.com>
2022-10-03 14:32:38 +0200

    Inicio de hacer el formulario

--- src/app/components/add-edit-empleado/add-edit-empleado.component.ts
+++ src/app/components/add-edit-empleado/add-edit-empleado.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-add-edit-empleado',
+  selector: 'add-edit-empleado',
   templateUrl: './add-edit-empleado.component.html',
   styleUrls: ['./add-edit-empleado.component.css']
 })
